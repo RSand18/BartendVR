cc62ec39b419eeaac68f5d468b1949ed300da924
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2020-03-04 10:56:51 -0600

    Added scripts folder and added 2 new scripts for recipe mixing

---
+++ Bartend VR/Assets/Scripts/DetectionManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionManager : MonoBehaviour
+{
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Orange")
+        {
+            ValueManager.value += 1;
+        }
+    }
+}
---
+++ Bartend VR/Assets/Scripts/ValueManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueManager : MonoBehaviour
+{
+    public static int value = 0;
+    public GameObject MartiniGlass_Unmixed, Orange, MartiniGlass_Mixed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MartiniGlass_Unmixed.SetActive(true);
+        Orange.SetActive(true);
+        MartiniGlass_Mixed.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (value == 1)
+        {
+            MartiniGlass_Unmixed.SetActive(false);
+            Orange.SetActive(false);
+            MartiniGlass_Mixed.SetActive(true);
+        }
+    }
+}

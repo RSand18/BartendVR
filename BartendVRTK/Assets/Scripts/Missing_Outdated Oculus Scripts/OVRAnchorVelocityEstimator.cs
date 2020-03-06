48da1ad30d382c6783f23fe2c91991ba0d417fac
SanGroku <37126121+SanGroku@users.noreply.github.com>
2020-03-06 01:12:22 -0600

    Interaction Setup
    - Created a script for OVRInputAxis1DAction. This was a scriipt mentioned in the tutorial that was missing from unity.
    - Made glasses on the bar grabbable.
    - Added velocity. Objects should be throwable now.

---
+++ BartendVRTK/Assets/Scripts/Missing_Outdated Oculus Scripts/OVRAnchorVelocityEstimator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Zinnia.Tracking.Velocity;
+
+public class OVRAnchorVelocityEstimator : VelocityTracker
+{
+    [Tooltip("The anchor from the OVRCameraRig to track velocity for.")]
+    public GameObject trackedGameObject;
+    [Tooltip("An optional object to consider the source relative to when retrieving velocities.")]
+
+    public GameObject relativeTo;
+
+    public override bool IsActive()
+    {
+        return trackedGameObject != null && trackedGameObject.activeInHierarchy && isActiveAndEnabled;
+    }
+
+    protected override Vector3 DoGetAngularVelocity()
+    {
+        switch (trackedGameObject.name)
+        {
+            case "CenterEyeAnchor":
+                return trackedGameObject.transform.rotation * (OVRManager.isHmdPresent ? OVRPlugin.GetNodeAngularVelocity(OVRPlugin.Node.EyeCenter, OVRPlugin.Step.Render).FromFlippedZVector3f() : Vector3.zero);
+            case "LeftHandAnchor":
+                return trackedGameObject.transform.rotation * OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+            case "RightHandAnchor":
+                return trackedGameObject.transform.rotation * OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+        }
+        return Vector3.zero;
+    }
+
+    protected override Vector3 DoGetVelocity()
+    {
+        switch (trackedGameObject.name)
+        {
+            case "CenterEyeAnchor":
+                return relativeTo.transform.rotation * (OVRManager.isHmdPresent ? OVRPlugin.GetNodeAngularVelocity(OVRPlugin.Node.EyeCenter, OVRPlugin.Step.Render).FromFlippedZVector3f() : Vector3.zero);
+            case "LeftHandAnchor":
+                return relativeTo.transform.rotation * OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+            case "RightHandAnchor":
+                return relativeTo.transform.rotation * OVRInput.GetLocalControllerAngularVelocity(OVRInput.Controller.LTouch);
+        }
+        return Vector3.zero;
+
+    }
+}
---
+++ BartendVRTK/Assets/Scripts/Missing_Outdated Oculus Scripts/OVRInputAxis1DAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Action;
+
+//Glue code that allows Oculus to interact with VRTK
+public class OVRInputAxis1DAction : FloatAction
+{
+    public OVRInput.Controller controller = OVRInput.Controller.Active;
+    public OVRInput.Axis1D axis;
+
+    void Update()
+    {
+        Receive(OVRInput.Get(axis, controller));
+    }
+}

f4545493a79cd912622008d254406f640f2ff75b
RSand18 <43037672+RSand18@users.noreply.github.com>
2020-03-06 16:06:53 -0600

    Got Bottles Pouring Along Y Axis

--- ParticleSystemImport/Assets/Scripts/PourDetector.cs
+++ ParticleSystemImport/Assets/Scripts/PourDetector.cs
@@ -9,16 +9,31 @@ public class PourDetector : MonoBehaviour
 
     private bool isPouring = false;
     private Stream currentStream = null;
-    
+
     private void Update()
     {
-        bool pourCheck = CalculatePourAngle() < pourThreshold;
+        bool pourCheck = CalculatePourAngle() > pourThreshold;
+        bool negpourCheck = negCalculatePourAngle() > pourThreshold;
 
-        if(isPouring != pourCheck)
+        if (isPouring != pourCheck)
         {
             isPouring = pourCheck;
 
             if(isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+
+        if (isPouring != negpourCheck)
+        {
+            isPouring = negpourCheck;
+
+            if (isPouring)
             {
                 StartPour();
             }
@@ -39,6 +54,8 @@ public class PourDetector : MonoBehaviour
     private void EndPour()
     {
         print("End");
+        currentStream.End();
+        currentStream = null;
     }
 
     private float CalculatePourAngle()
@@ -46,10 +63,14 @@ public class PourDetector : MonoBehaviour
         return transform.forward.y * Mathf.Rad2Deg;
     }
 
+    private float negCalculatePourAngle()
+    {
+        return transform.forward.y * -1 * Mathf.Rad2Deg;
+    }
+
     private Stream CreateStream()
     {
         GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
         return streamObject.GetComponent<Stream>();
     }
-    
 }
--- ParticleSystemImport/Assets/Scripts/Stream.cs
+++ ParticleSystemImport/Assets/Scripts/Stream.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Stream : MonoBehaviour
 {
     private LineRenderer lineRenderer = null;
+    private ParticleSystem splashParticle = null;
 
+    private Coroutine pourRoutine = null;
     private Vector3 targetPosition = Vector3.zero;
 
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        splashParticle = GetComponentInChildren<ParticleSystem>();
     }
 
-    void Start()
+    private void Start()
     {
         MoveToPosition(0, transform.position);
         MoveToPosition(1, transform.position);
@@ -21,7 +24,8 @@ public class Stream : MonoBehaviour
 
     public void Begin()
     {
-        StartCoroutine(BeginPour());
+        StartCoroutine(UpdateParticle());
+        pourRoutine = StartCoroutine(BeginPour());
     }
 
     private IEnumerator BeginPour()
@@ -31,10 +35,29 @@ public class Stream : MonoBehaviour
             targetPosition = FindEndPoint();
 
             MoveToPosition(0, transform.position);
-            MoveToPosition(1, targetPosition);
+            AnimateToPosition(1, targetPosition);
 
             yield return null;
         }
+    }
+
+    public void End()
+    {
+        StopCoroutine(pourRoutine);
+        pourRoutine = StartCoroutine(EndPour());
+    }
+
+    private IEnumerator EndPour()
+    {
+        while(!HasReachedPosition(0, targetPosition))
+        {
+            AnimateToPosition(0, targetPosition);
+            AnimateToPosition(1, targetPosition);
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 
     private Vector3 FindEndPoint()
@@ -51,5 +74,31 @@ public class Stream : MonoBehaviour
     private void MoveToPosition(int index, Vector3 targetPosition)
     {
         lineRenderer.SetPosition(index, targetPosition);
+    }
+
+    private void AnimateToPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPoint = lineRenderer.GetPosition(index);
+        Vector3 newPosition = Vector3.MoveTowards(currentPoint, targetPosition, Time.deltaTime * 1.75f);
+        lineRenderer.SetPosition(index, newPosition);
+    }
+
+    private bool HasReachedPosition(int index, Vector3 targetPosition)
+    {
+        Vector3 currentPosition = lineRenderer.GetPosition(index);
+        return currentPosition == targetPosition;
+    }
+
+    private IEnumerator UpdateParticle()
+    {
+        while(gameObject.activeSelf)
+        {
+            splashParticle.gameObject.transform.position = targetPosition;
+
+            bool isHitting = HasReachedPosition(1, targetPosition);
+            splashParticle.gameObject.SetActive(isHitting);
+
+            yield return null;
+        }
     }
 }

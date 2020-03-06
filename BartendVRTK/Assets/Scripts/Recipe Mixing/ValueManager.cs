f37c87485498eedcd61026d354d7d06b8d67889d
Athena Roberts-Forouzi <Athena.RobertsForouzi@iam.local>
2020-03-06 14:53:34 -0600

    Add mixable items
    -Adds an orange and a lemon model.
    - Player can "mix" the two fruits into the glasses on the bar (martini glass and beer glass).
    - Glasses change color when they interact with the fruits.
    - Updated recipe mixing scripts.
    _ Adjusted the player camera and player start area.

---
+++ BartendVRTK/Assets/Scripts/Recipe Mixing/ValueManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueManager : MonoBehaviour
+{
+    public static int value = 0;
+    public GameObject MartiniGlass_Unmixed, MartiniGlass_Mixed, BeerGlass_Unmixed, BeerGlass_Mixed, Orange, Lemon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MartiniGlass_Unmixed.SetActive(true);
+        BeerGlass_Unmixed.SetActive(true);
+        MartiniGlass_Mixed.SetActive(false);
+        BeerGlass_Mixed.SetActive(false);
+        Orange.SetActive(true);
+        Lemon.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (value == 1)
+        {
+            Debug.Log("Value has been increased");
+            Debug.Log("Orange Hidden");
+            MartiniGlass_Mixed.SetActive(true);
+            Debug.Log("Orange Martini Glass active");
+        }
+
+        if (value == 2)
+        {
+            BeerGlass_Mixed.SetActive(true);
+            Debug.Log("Yellow Beer Glass active");
+        }
+    }
+}

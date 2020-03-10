5c17df68d0c3b24f89154a67dab9063be8128d1f
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2020-03-10 14:53:00 -0500

    Added 2 ingridient recipe mixing script

---
+++ BartendVRTK/Assets/Scripts/Recipe Mixing/DetectionManager_MartiniGlassMixed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionManager_MartiniGlassMixed : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Lemon")
+        {
+            ValueManager.value = 3;
+            Debug.Log("Value is 3");
+            Destroy(GameObject.FindWithTag("Lemon"));
+            Destroy(GameObject.FindWithTag("MartiniGlass_Mixed"));
+        }
+    }
+
+}
--- BartendVRTK/Assets/Scripts/Recipe Mixing/ValueManager.cs
+++ BartendVRTK/Assets/Scripts/Recipe Mixing/ValueManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class ValueManager : MonoBehaviour
 {
     public static int value = 0;
-    public GameObject MartiniGlass_Unmixed, MartiniGlass_Mixed, BeerGlass_Unmixed, BeerGlass_Mixed, Orange, Lemon;
+    public GameObject MartiniGlass_Unmixed, MartiniGlass_Mixed, MartiniGlass_Mixed_Lemon, BeerGlass_Unmixed, BeerGlass_Mixed, Orange, Lemon;
     // Start is called before the first frame update
     void Start()
     {
         MartiniGlass_Unmixed.SetActive(true);
         BeerGlass_Unmixed.SetActive(true);
+        MartiniGlass_Mixed_Lemon.SetActive(false);
         MartiniGlass_Mixed.SetActive(false);
         BeerGlass_Mixed.SetActive(false);
         Orange.SetActive(true);
@@ -22,16 +23,19 @@ public class ValueManager : MonoBehaviour
     {
         if (value == 1)
         {
-            Debug.Log("Value has been increased");
-            Debug.Log("Orange Hidden");
             MartiniGlass_Mixed.SetActive(true);
-            Debug.Log("Orange Martini Glass active");
         }
 
         if (value == 2)
         {
             BeerGlass_Mixed.SetActive(true);
-            Debug.Log("Yellow Beer Glass active");
+        }
+
+        if (value == 3)
+        {
+            MartiniGlass_Mixed.SetActive(false);
+            Debug.Log("Reached");
+            MartiniGlass_Mixed_Lemon.SetActive(true);
         }
     }
 }

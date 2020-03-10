578a0910471ff2368b135a68ef11ed40cf8e0557
Ishan Dutta <ishan.dutta2006@yahoo.co.in>
2020-03-10 14:17:32 -0500

    Made 2 seperate scripts that handle recipe mixing preventing any trigger issues

---
+++ BartendVRTK/Assets/Scripts/Recipe Mixing/DetectionManagerBeerGlass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionManagerBeerGlass : MonoBehaviour
+{
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Lemon")
+        {
+            ValueManager.value = 2;
+            Debug.Log("Value is 2");
+            Destroy(GameObject.FindWithTag("Lemon"));
+            Destroy(GameObject.FindWithTag("BeerGlass_Unmixed"));
+        }
+    }
+}
---
+++ BartendVRTK/Assets/Scripts/Recipe Mixing/DetectionManagerMartiniGlass.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionManagerMartiniGlass : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Orange")
+        {
+            ValueManager.value = 1;
+            Debug.Log("Value is 1");
+            Destroy(GameObject.FindWithTag("Orange"));
+            Destroy(GameObject.FindWithTag("MartiniGlass_Unmixed"));
+        }
+    }
+}
